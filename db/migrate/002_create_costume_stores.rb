ee109b44b691c43565baae5dee3e3e622dfcf3fd
Brian Simonton <bsimonton78@gmail.com>
2019-04-24 22:12:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
   
   
   def change
+    
     create_table :costume_stores do |t|
       
       t.string :name
@@ -12,9 +13,9 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
-    
-    
     end
-  
+    
+    
   end
+  
 end
