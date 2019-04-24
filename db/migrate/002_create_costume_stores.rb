2859ebe3600a6351a26ef3fa00b719ad1889de15
Brian Simonton <bsimonton78@gmail.com>
2019-04-24 22:09:28 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
+      t.integer :employee_count
+      t.boolean :closed? 
       t.datetime :opening_time
       t.datetime :closing_time
     
